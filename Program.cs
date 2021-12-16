eb9557119c2f39ca647257b21742cb5cd92df1c5
Asafe kauffmann <dev.kauffmann@gmail.com>
2021-12-16 12:20:18 -0300

    up

--- Program.cs
+++ Program.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace C_
-{
-   public class Program
-   {
-      test test = new test();
 
-      public static void Main(string[] args, test test)
-      {
-         Console.WriteLine("Hello World!");
-         test.TesteMetodo();
+namespace C_ {
+   public class Program {
+      
+      static void Main( string[] args ) {
+
+         int n1 = int.Parse(Console.ReadLine());
+
+         char ch = char.Parse(Console.ReadLine());
+
+         Console.WriteLine("e ai");
+
+         Console.WriteLine("Você digitou: ");
+         Console.WriteLine(n1);
+         Console.WriteLine(ch);
+
+
+
+
       }
    }
 }
