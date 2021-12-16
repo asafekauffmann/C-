0e278198816431fc61f71b120f98f912e27002f5
Asafe kauffmann <dev.kauffmann@gmail.com>
2021-12-16 11:26:31 -0300

    up

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace C_
+{
+   public class Program
+   {
+      public static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
+
+
+// See https://aka.ms/new-console-template for more information
